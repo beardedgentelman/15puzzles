817284de2a24455e65e67e75dec55dd066c69fe7
Andrii <81422574+beardedgentelman@users.noreply.github.com>
2022-10-02 16:31:44 +0300

    v2.1

--- js/main.js
+++ js/main.js
@@ -1,29 +1,47 @@
 window.addEventListener('DOMContentLoaded', () => {
     const puzzles = document.querySelectorAll('.puzzle__item')
     const puzzlesArray = Array.from(puzzles);
-    const zeroPuzzle = puzzlesArray[0];
-    const puzzleArea = document.querySelector('.main__puzzle');
-    
-    for (let i = 0; i < puzzlesArray.length; i++) {
-        const puzNum = {puzNum : i};
-        Object.assign(puzzlesArray[i], puzNum);
-    }
-
-    const key = 'puzNum';
-
-    puzzlesArray.forEach((el, i) => {
-        el.addEventListener('click', () => {
-            let clickPuzNum = el.puzNum;
-
-            el.puzNum = zeroPuzzle.puzNum;
-            zeroPuzzle.puzNum = clickPuzNum;
-
-            const sortedPuzzles = puzzlesArray.sort((el, zeroPuzzle) => el[key] > zeroPuzzle[key] ? 1 : -1);
-            console.log(sortedPuzzles);
-
-            $(puzzleArea).empty();
-            $(puzzleArea).append(sortedPuzzles);
+    let timeZero;
+    function findZeroCoord() {
+        timeZero = $(puzzlesArray[0]).offset({
+            top: this.top,
+            left: this.left
         });
+    };
+    
+    findZeroCoord();
+
+    console.log(timeZero.position().left);
+    $(puzzlesArray).each(function (index) {
+        if(index !== 0){
+            $(this).click(function() {
+                const currentEl = $(this);
+                // Найти координаты кликнутой ячейки \ Find the coordinates of the clicked cell
+                function findCurrentCoord() {
+                    timeCurrent = currentEl.offset({
+                        top: currentEl.top,
+                        left: currentEl.left
+                    });
+
+                    return timeCurrent.position();
+                };
+                findCurrentCoord();
+
+                // Запись координат во временные переменные \ Writing coordinates to temporary variables
+                CurrTop = currentEl.offset().top;
+                CurrLeft = currentEl.offset().left;
+                
+                if ((timeCurrent.position().left - 100) === timeZero.position().left) {
+                    $(puzzlesArray[0]).insertAfter(currentEl);
+                } 
+                else if ((timeCurrent.position().left + 100) === timeZero.position().left) {
+                    $(puzzlesArray[0]).insertBefore(currentEl);
+                }
+                console.log(timeCurrent.position().left);
+                console.log(timeZero.position().left);
+
+            });
+        };
     });
     
 });
